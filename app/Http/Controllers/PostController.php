95fd51d0940a34fc82907d9613ce632e5f0e37ca
codeintheshell <thibault-castelli@users.noreply.github.com>
2025-07-03 15:44:45 +0200

    Clean up navigation layout

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     {
         $posts = Post::orderBy('created_at', 'desc')->simplePaginate(5);
 
-        return view('dashboard', [
+        return view('post.index', [
             'posts' => $posts,
         ]);
     }
