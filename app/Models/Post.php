41563cc056ca2d51a183255771d317128fd99a3d
codeintheshell <thibault-castelli@users.noreply.github.com>
2025-07-03 14:08:42 +0200

    Initialize repository

---
+++ app/Models/Post.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Post
+ *
+ * @mixin Builder
+ * @property int $id
+ * @property string|null $image
+ * @property string $title
+ * @property string $slug
+ * @property string $content
+ * @property int $category_id
+ * @property int $user_id
+ * @property string|null $published_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PostFactory factory($count = null, $state = [])
+ * @method static Builder<static>|Post newModelQuery()
+ * @method static Builder<static>|Post newQuery()
+ * @method static Builder<static>|Post query()
+ * @method static Builder<static>|Post whereCategoryId($value)
+ * @method static Builder<static>|Post whereContent($value)
+ * @method static Builder<static>|Post whereCreatedAt($value)
+ * @method static Builder<static>|Post whereId($value)
+ * @method static Builder<static>|Post whereImage($value)
+ * @method static Builder<static>|Post wherePublishedAt($value)
+ * @method static Builder<static>|Post whereSlug($value)
+ * @method static Builder<static>|Post whereTitle($value)
+ * @method static Builder<static>|Post whereUpdatedAt($value)
+ * @method static Builder<static>|Post whereUserId($value)
+ * @mixin \Eloquent
+ */
+class Post extends Model
+{
+    use HasFactory;
+}
